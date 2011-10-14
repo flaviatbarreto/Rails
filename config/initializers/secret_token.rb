78ce7d3d81b89d796caead23728066fe43913a1e
InfoJr04 <infojr05@ubuntu.(none)>
2011-10-14 10:13:09 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails2::Application.config.secret_token = 'efbfb6edb6ec1e276ede248a761229b2bccf44ef6ce940e2d138404d9ab2109c1d5bc832a0b92deafbfb99466fcc85bcc76c29eb357d7c8918ac2eb9ccbfcb1e'
